cdb5cd84fd5679cdc43c3c0954b434604cc24cfc
SilverD3 <mrboornesilver@gmail.com>
2020-04-10 14:02:19 +0100

    challenge1 fix bug4

--- src/estimator.php
+++ src/estimator.php
@@ -1,41 +1,18 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 require_once "objects/severImpact.php";
 require_once "objects/impact.php";
-
-//creating a mock data
-$data = json_encode(array(
-	"region" => array(
-		"name" => "Africa",
-		"avgAge" => 20,
-		"avgDailyIncomeInUSD" => 5,
-		"avgDailyIncomePopulation" => 1
-	),
-	"periodType" => "days",
-	"timeToElapse" => 50,
-	"reportedCases" => 800,
-	"population" => 26000000,
-	"totalHospitalBeds" => 1380614
-));
 
 function covid19ImpactEstimator($input_data)
 {
 	$impact = new Impact();
 	$severImpact = new SeverImpact();
   	$outputData = [
-  		'data' => json_decode($input_data), 
+  		'data' => json_decode(json_encode($input_data)), 
   		'estimate' =>[
-	  		'impact' => $impact->computeImpact(json_decode($input_data)), 
-	  		'severeImpact' => $severImpact->computeSeverImpact(json_decode($input_data))
+	  		'impact' => $impact->computeImpact(json_decode(json_encode($input_data))), 
+	  		'severeImpact' => $severImpact->computeSeverImpact(json_decode(json_encode($input_data)))
 	  	]
   	];
-  	return $outputData;
+  	return json_encode($outputData);
 }
-
-echo json_encode(covid19ImpactEstimator($data));
