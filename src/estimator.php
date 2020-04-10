158bb90cc8f41a20a220c28533cdbf8b251a2eeb
SilverD3 <mrboornesilver@gmail.com>
2020-04-10 11:54:50 +0100

    challenge1 fix bug

--- src/estimator.php
+++ src/estimator.php
@@ -9,7 +9,20 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 require_once "objects/severImpact.php";
 require_once "objects/impact.php";
 
-$data = json_decode(file_get_contents('php://input'));
+//creating a mock data
+$data = json_decode(json_encode(array(
+	"region" => array(
+		"name" => "Africa",
+		"avgAge" => 20,
+		"avgDailyIncomeInUSD" => 5,
+		"avgDailyIncomePopulation" => 1
+	),
+	"periodType" => "days",
+	"timeToElapse" => 50,
+	"reportedCases" => 800,
+	"population" => 26000000,
+	"totalHospitalBeds" => 1380614
+)));
 
 function covid19ImpactEstimator($data)
 {
