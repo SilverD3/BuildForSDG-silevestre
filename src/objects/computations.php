1bc10d40887a1f80efb8e508a6004095db48ffd8
SilverD3 <mrboornesilver@gmail.com>
2020-04-13 17:47:22 +0100

    fix all issues1

--- src/objects/computations.php
+++ src/objects/computations.php
@@ -12,10 +12,10 @@ class Computations
 				return $timeToElapse;
 				break;
 			case 'weeks':
-				return 7 * $timeToElapse; //7 is number of days in a week
+				return (7 * $timeToElapse); //7 is number of days in a week
 				break;
 			case 'months':
-				return 30 * $timeToElapse; // 30 is number of days in a month
+				return (30 * $timeToElapse); // 30 is number of days in a month
 				break;
 			default:
 				return $timeToElapse;
@@ -30,29 +30,29 @@ class Computations
 	function infectionsByRequestedTime($currentlyInfected, $data){
 		$factor = round(($this->normalizeDuration($data->periodType, $data->timeToElapse)/3), 0);
 		$infectionsByRequestedTime = $currentlyInfected * pow(2, $factor);
-		return round($infectionsByRequestedTime, 0);
+		return number_format($infectionsByRequestedTime, 0, '.', '');
 	}
 
 	function severeCasesByRequestedTime($infectionsByRequestedTime){
-		$severeCasesByRequestedTime = 15 * $infectionsByRequestedTime / 100;
+		$severeCasesByRequestedTime = $infectionsByRequestedTime * 0.15;
 		return round($severeCasesByRequestedTime, 0);
 	}
 
 	function hospitalBedsByRequestedTime($totalHospitalBeds, $severeCasesByRequestedTime){
-		$hospitalBedsBRT = (35 * $totalHospitalBeds / 100) - $severeCasesByRequestedTime;
+		$hospitalBedsBRT = ($totalHospitalBeds * 0.35) - $severeCasesByRequestedTime;
 		return round($hospitalBedsBRT, 0);
 	}
 
 	function casesForICUByRequestedTime($infectionsByRequestedTime){
-		return round((5 * $infectionsByRequestedTime / 100), 0);
+		return round($infectionsByRequestedTime * 0.05, 0);
 	}
 
 	function casesForVentilatorsByRequestedTime($infectionsByRequestedTime){
-		return round((2 * $infectionsByRequestedTime / 100), 0);
+		return round($infectionsByRequestedTime * 0.02, 0);
 	}
 
 	function dollarsInFlight($infectionsByRequestedTime, $data){
-		$dollarsInFlight = $infectionsByRequestedTime * $data->region->avgDailyIncomeInUSD * $data->region->avgDailyIncomePopulation * $this->normalizeDuration($data->periodType, $data->timeToElapse);
+		$dollarsInFlight = $infectionsByRequestedTime * ($data->region->avgDailyIncomeInUSD / $this->normalizeDuration($data->periodType, $data->timeToElapse))* $data->region->avgDailyIncomePopulation;
 
 		return round($dollarsInFlight, 0);
 	}
